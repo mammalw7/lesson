aa7e071aac704b6a1a6e73bc9959a683df59c8d6
mammalw7 <145951114+mammalw7@users.noreply.github.com>
2023-11-02 21:57:11 +0800

    数字三角形

---
+++ 数字三角形模型/数字三角形.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+const int N = 10010 , INF = 1e9 + 10;
+int f[N][N] , a[N][N];
+int n;
+int main()
+{
+    cin >> n;
+    for(int i = 1 ; i <= n ; i ++ )
+        for(int j = 1 ; j <= i ; j ++ )
+            cin >> a[i][j];
+    for(int i = 0 ; i <= n ; i ++ )
+        for(int j = 0 ; j <= i + 1 ; j ++ )
+            f[i][j] = -INF;
+    f[1][1] = a[1][1];
+    for(int i = 2 ; i <= n ; i ++ )
+        for(int j = 1 ; j <= i ; j ++ )
+            f[i][j] = max(f[i - 1][j - 1] + a[i][j] , f[i - 1][j] + a[i][j]);
+    int res = -INF;
+    for(int i = 1 ; i <= n ; i ++ )         res = max(res , f[n][i]);
+    cout << res << endl;
+    return 0;
+}
