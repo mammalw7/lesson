65a96b62434f36e6ad2d2d249c6e69e13a77c593
mammalw7 <145951114+mammalw7@users.noreply.github.com>
2023-11-06 23:04:57 +0800

    最大上升子序列和

---
+++ 最长上升子序列模型/最大上升子序列和.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+const int N = 1100;
+int a[N];
+int f[N];
+int n;
+int main()
+{
+    cin >> n;
+    for (int i = 1 ; i <= n ; i ++ )        cin >> a[i];
+    int res = 0;
+    for (int i = 1 ; i <= n ; i ++ )
+    {
+        f[i] = a[i];
+        for (int j = 1 ; j < i ; j ++ )
+            if(a[j] < a[i])
+                f[i] = max (f[i] , f[j] + a[i]);
+    }
+    for (int i = 1 ; i <= n ; i ++ )    res = max (res , f[i]);
+    cout << res << endl;
+    return 0;
+}
